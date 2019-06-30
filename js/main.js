fd4e728d7d43bf39d4db34a612bf35afcee0377d
valentinavega <valentinavega94@gmail.com>
2019-06-30 19:31:24 -0400

    Lots of updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+//Scroll to top button on Case Study pages
+
 var btn = $('#scroll-to-top');
 
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
+    if ($(window).scrollTop() > 600) {
         btn.addClass('show');
     } else {
         btn.removeClass('show');
@@ -12,3 +14,21 @@ btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+// This makes it so that each Section appears on scroll-- thereby making it easier for readers of the case study to see that it is an entirely new section of the design process.
+
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible")
+        } else {
+            $(tag).removeClass("visible")
+        }
+    }
+})
