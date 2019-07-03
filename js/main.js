2ad54085a60cbe2de1245fb446b4c762d8a20ae8
valentinavega <valentinavega94@gmail.com>
2019-07-03 00:32:12 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -14,22 +14,3 @@ btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
-
-// This makes it so that each Section appears on scroll-- thereby making it easier for readers of the case study to see that it is an entirely new section of the design
-
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop()
-    var pageBottom = pageTop + $(window).height()
-    var tags = $(".fade-image")
-
-
-    for (var i = 0; i < tags.length; i++) {
-        var tag = tags[i]
-
-        if ($(tag).position().top < pageBottom) {
-            $(tag).addClass("visible")
-        } else {
-            $(tag).removeClass("visible")
-        }
-    }
-})
