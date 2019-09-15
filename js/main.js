5cbde2692ae48ea577d75ca187bf2a9fba37edb2
valentinavega <valentinavega94@gmail.com>
2019-09-15 07:31:28 -0700

    more updates

--- js/main.js
+++ js/main.js
@@ -14,24 +14,3 @@ btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
-
-
-highlight();
-
-$(window).on("scroll", function() {
-    highlight();
-});
-
-function highlight() {
-    var scroll = $(window).scrollTop();
-    var height = $(window).height();
-
-    $(".highlight").each(function() {
-        var pos = $(this).offset().top;
-        if (scroll + height >= pos) {
-            $(this).addClass("active");
-        }
-        console.log(pos);
-        console.log(scroll);
-    });
-}
